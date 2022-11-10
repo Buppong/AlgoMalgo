ad522d5e63b54a886404fc20244eb934b64174da
chanbokLee <changbok2453@gmail.com>
2022-11-10 22:17:04 +0900

    testing

---
+++ 스택수열.cpp
@@ -0,0 +1,30 @@
+#include <isotream>
+#include <stack>
+#include <string>
+
+
+
+
+
+using namespace std;
+
+int main() {
+    stack<int> s;
+    int n;
+    string ans;
+
+    cin >> n;
+
+
+    int m = 0;
+
+    while( n--){
+        int x;
+        cin >> x;
+        e
+    }
+
+
+
+
+}
